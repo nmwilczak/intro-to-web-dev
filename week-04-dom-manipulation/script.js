5ff7d0cefc8efa460804a2d9236002a0187c4e29
nmwilczak <98558301+nmwilczak@users.noreply.github.com>
2022-02-18 13:07:46 -0500

    sad boi hours

--- week-04-dom-manipulation/script.js
+++ week-04-dom-manipulation/script.js
@@ -17,4 +17,72 @@ function fillOutputs() {
   const outputDiv = document.getElementById('output');
   // Make the output container element visible
   outputDiv.style.display = 'block';
+
+
+
+
+  // Get the adjective input element
+  const samenameOneInput = document.getElementById('samename-one-input');
+  // Get the text from the input element
+  const samenameOne = samenameOneInput.value;
+
+  // Get the adjective output element
+  const samenameOneOutput = document.getElementById('samename-one-output');
+  // Set the text of the input element
+  samenameOneOutput.innerText = samenameOne;
+
+
+
+
+  // Get the adjective input element
+  const locationOneInput = document.getElementById('location-one-input');
+  // Get the text from the input element
+  const locationOne = locationOneInput.value;
+
+  // Get the adjective output element
+  const locationOneOutput = document.getElementById('location-one-output');
+  // Set the text of the input element
+  locationOneOutput.innerText = locationOne;
+
+
+
+  // Get the adjective input element
+  const pOneInput = document.getElementById('p-one-input');
+  // Get the text from the input element
+  const pOne = pOneInput.value;
+
+  // Get the adjective output element
+  const pOneOutput = document.getElementById('p-one-output');
+  // Set the text of the input element
+  pOneOutput.innerText = pOne;
+
+
+
+
+
+  // Get the adjective input element
+  const nounOneInput = document.getElementById('noun-one-input');
+  // Get the text from the input element
+  const nounOne = nounOneInput.value;
+
+  // Get the adjective output element
+  const nounOneOutput = document.getElementById('noun-one-output');
+  // Set the text of the input element
+  nounOneOutput.innerText = nounOne;
+
+
+
+    // Get the adjective input element
+  const noun2OneInput = document.getElementById('noun2-one-input');
+  // Get the text from the input element
+  const noun2One = noun2OneInput.value;
+
+  // Get the adjective output element
+  const noun2OneOutput = document.getElementById('noun2-one-output');
+  // Set the text of the input element
+  noun2OneOutput.innerText = noun2One;
+
+
 }
+
+
