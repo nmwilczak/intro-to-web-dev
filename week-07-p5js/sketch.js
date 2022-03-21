11b2b1ecf1da67303f20ed1b7530e61a324d4aff
nmwilczak <98558301+nmwilczak@users.noreply.github.com>
2022-03-20 23:06:23 -0400

    ah

--- week-07-p5js/sketch.js
+++ week-07-p5js/sketch.js
@@ -7,6 +7,12 @@ let t5= 0;
 let t6= 0;
 let t7= 0;
 
+let cx, cy;
+let secondsRadius;
+let minutesRadius;
+let hoursRadius;
+let clockDiameter;
+
 
 let fall=0;
 
@@ -16,10 +22,24 @@ function setup() {
   background(255, 239, 194);
   textSize(23);
   noStroke();
+
+
+
+
+  let radius = min(width, height) / 4;
+  secondsRadius = radius * 0.71;
+  minutesRadius = radius * 0.6;
+  hoursRadius = radius * 0.5;
+  clockDiameter = radius * 1.7;
+
+  cx = width / 2;
+  cy = height / 2;
 }
 
 
 function draw() {
+
+
 
 
 
@@ -28,6 +48,49 @@ function draw() {
   let ix = width - mouseX;  // Inverse X
   let iy = height - mouseY; // Inverse Y
   background(255, 239, 194);
+
+    drawTarget(width * 0.25, height * 0.4, 200, 4);
+  drawTarget(width * 0.5, height * 0.5, 300, 10);
+  drawTarget(width * 0.75, height * 0.3, 120, 6);
+
+
+
+     noStroke();
+  fill(244, 122, 158);
+  ellipse(cx, cy, clockDiameter + 25, clockDiameter + 25);
+  fill(237, 34, 93);
+  ellipse(cx, cy, clockDiameter, clockDiameter);
+
+  // Angles for sin() and cos() start at 3 o'clock;
+  // subtract HALF_PI to make them start at the top
+  let s = map(second(), 0, 60, 0, TWO_PI) - HALF_PI;
+  let m = map(minute() + norm(second(), 0, 60), 0, 60, 0, TWO_PI) - HALF_PI;
+  let h = map(hour() + norm(minute(), 0, 60), 0, 24, 0, TWO_PI * 2) - HALF_PI;
+
+  // Draw the hands of the clock
+  stroke(255);
+  strokeWeight(1);
+  line(cx, cy, cx + cos(s) * secondsRadius, cy + sin(s) * secondsRadius);
+  strokeWeight(2);
+  line(cx, cy, cx + cos(m) * minutesRadius, cy + sin(m) * minutesRadius);
+  strokeWeight(4);
+  line(cx, cy, cx + cos(h) * hoursRadius, cy + sin(h) * hoursRadius);
+
+  // Draw the minute ticks
+  strokeWeight(2);
+  beginShape(POINTS);
+  for (let a = 0; a < 360; a += 6) {
+    let angle = radians(a);
+    let x = cx + cos(angle) * secondsRadius;
+    let y = cy + sin(angle) * secondsRadius;
+    vertex(x, y);
+  }
+  endShape();
+
+
+
+
+
   fill(209, 209, 209);
   ellipse(x, height/2, y, y);
   fill(255, 203, 161);
@@ -164,5 +227,20 @@ function draw() {
 
 
 
+
+
+}
+
+
+
+
+
+function drawTarget(xloc, yloc, size, num) {
+  const color = 100 / num;
+  const steps = size / num;
+  for (let i = 0; i < num; i++) {
+    fill(i * color);
+    ellipse(xloc, yloc, size - i * steps, size - i * steps);
+  }
 }
 
